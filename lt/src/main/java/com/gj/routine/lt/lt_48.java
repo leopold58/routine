31833cee7ab9295f163631cadf76dbc5b235b4d3
suiyebuluo <guoju58@163.com>
2021-06-20 20:58:15 +0800

    旋转图像（pre）

---
+++ lt/src/main/java/com/gj/routine/lt/lt_48.java
@@ -0,0 +1,47 @@
+package com.gj.routine.lt;
+
+import java.util.Scanner;
+
+/**
+ * @title: 旋转图像
+ * @description: 给定一个 n×n 的二维矩阵matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ *               你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ *                输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ *                输出：[[7,4,1],[8,5,2],[9,6,3]]
+ *                输入：matrix = [[1,2],[3,4]]
+ *                输出：[[3,1],[4,2]]
+ *               提示：
+ *               matrix.length == n
+ *               matrix[i].length == n
+ *               1 <= n <= 20
+ *               -1000 <= matrix[i][j] <= 1000
+ * @author: jguo
+ * @date: 2021/6/18
+ */
+public class lt_48 {
+    public void rotate(int[][] matrix) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        int n = in.nextInt();
+        String[] sInput = s.split(",");
+        int[] numIn = new int[sInput.length];
+        for (int i = 0; i < sInput.length; i++) {
+            numIn[i] = Integer.parseInt(sInput[i]);
+        }
+        int[][] matrix = new int[n][n];
+        int k = 0;
+        System.out.println("输入的矩阵：");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = numIn[k++];
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println("输出的矩阵：");
+    }
+}
