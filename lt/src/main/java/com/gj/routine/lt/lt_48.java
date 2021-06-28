14f159cc87be62e4052bff17b8cbb36c825fde37
suiyebuluo <guoju58@163.com>
2021-06-28 21:35:13 +0800

    旋转图像

--- lt/src/main/java/com/gj/routine/lt/lt_48.java
+++ lt/src/main/java/com/gj/routine/lt/lt_48.java
@@ -20,12 +20,30 @@ import java.util.Scanner;
  */
 public class lt_48 {
     public void rotate(int[][] matrix) {
-
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("输入n*n个数字，采用逗号隔开：");
         String s = in.nextLine();
+        System.out.println("输入n：");
         int n = in.nextInt();
         String[] sInput = s.split(",");
         int[] numIn = new int[sInput.length];
@@ -43,5 +61,13 @@ public class lt_48 {
             System.out.println();
         }
         System.out.println("输出的矩阵：");
+        lt_48 solution = new lt_48();
+        solution.rotate(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
     }
 }
