9e63f9be8781d9a3cc594814cb2e040f0a7386f7
suiyebuluo <guoju58@163.com>
2021-07-12 23:56:45 +0800

    最后一个单词的长度

---
+++ lt/src/main/java/com/gj/routine/lt/lt_58.java
@@ -0,0 +1,43 @@
+package com.gj.routine.lt;
+
+import java.util.Scanner;
+
+/**
+ * @title: 最后一个单词的长度
+ * @description: 给你一个字符串 s，由若干单词组成，单词之间用空格隔开。返回字符串中最后一个单词的长度。如果不存在最后一个单词，请返回 0 。
+ *               单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+ *               示例 1：
+ *                  输入：s = "Hello World"
+ *                  输出：5
+ *               示例 2：
+ *                  输入：s = " "
+ *                  输出：0
+ *               提示：
+ *                  1 <= s.length <= 104
+ *                  s 仅有英文字母和空格 ' ' 组成
+ * @author: jguo
+ * @date: 2021/7/12
+ */
+public class lt_58 {
+
+    public int lengthOfLastWord(String s) {
+        if (s==null || s.length()==0){
+            return 0;
+        }
+        int end = s.length()-1;
+        while (end>=0 && s.charAt(end) == ' '){end--;}
+        if (end<0){return 0;}
+        int start = end;
+        while (start>=0 && s.charAt(start)!=' '){
+            start--;
+        }
+        return end-start;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        lt_58 solution = new lt_58();
+        System.out.println(solution.lengthOfLastWord(s));
+    }
+}
