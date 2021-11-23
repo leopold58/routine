d96c138ef64e7f99b33e8b0be71c77e98147665b
suiyebuluo <guoju58@163.com>
2021-11-23 23:12:03 +0800

    子集 II

--- lt/src/main/java/com/gj/routine/lt/lt_90.java
+++ lt/src/main/java/com/gj/routine/lt/lt_90.java
@@ -20,8 +20,35 @@ import java.util.*;
  */
 public class lt_90 {
 
+    List<List<Integer>> result = new ArrayList<>();// 存放符合条件结果的集合
+    LinkedList<Integer> path = new LinkedList<>();// 用来存放符合条件结果
+    boolean[] used;
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        return null;
+        if (nums.length == 0){
+            result.add(path);
+            return result;
+        }
+        Arrays.sort(nums);
+        used = new boolean[nums.length];
+        subsetsWithDupHelper(nums, 0);
+        return result;
+    }
+
+    private void subsetsWithDupHelper(int[] nums, int startIndex){
+        result.add(new ArrayList<>(path));
+        if (startIndex >= nums.length){
+            return;
+        }
+        for (int i = startIndex; i < nums.length; i++){
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]){
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = true;
+            subsetsWithDupHelper(nums, i + 1);
+            path.removeLast();
+            used[i] = false;
+        }
     }
 
 
