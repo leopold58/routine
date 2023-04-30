4774f36e2538aa08ca70b2781529adb008b1dc70
leopold58 <guoju58@163.com>
2023-04-30 16:07:31 +0800

    岛屿数量 深度优先

---
+++ lt/src/main/java/com/gj/routine/lt/lt_200.java
@@ -0,0 +1,45 @@
+package com.gj.routine.lt;
+
+public class lt_200 {
+
+    public int numIslands(char[][] grid){
+        if (grid == null || grid.length == 0){
+            return 0;
+        }
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int numOfIslands = 0;
+        for (int r=0;r<nr;r++){
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == '1'){
+                    numOfIslands++;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return numOfIslands;
+    }
+    public void dfs(char[][] grid, int r, int c){
+        int nr = grid.length;
+        int nc = grid[0].length;
+        if (r<0 || c<0 || r>= nr || c >= nc || grid[r][c] == '0'){
+            return;
+        }
+        grid[r][c] = '0';
+        dfs(grid, r-1, c);
+        dfs(grid, r+1, c);
+        dfs(grid, r, c-1);
+        dfs(grid, r, c+1);
+    }
+    public static void main(String[] args) {
+        lt_200 solution = new lt_200();
+        char[][] grid = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        int res = solution.numIslands(grid);
+        System.out.println(res);
+    }
+}
